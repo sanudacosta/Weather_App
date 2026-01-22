74a66ae5f7d1f39b02cf21d8fb88cb524852480d
sanudacosta <sanudacosta@gmail.com>
2026-01-22 11:57:35 +0530

    Add dependencies and setup project structure

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,15 @@
+// API Constants
+class ApiConstants {
+  static const String baseUrl = 'https://api.openweathermap.org/data/2.5';
+  static const String weatherEndpoint = '/weather';
+  static const String forecastEndpoint = '/forecast';
+  
+  // Add your API key here
+  static const String apiKey = 'YOUR_API_KEY_HERE';
+}
+
+// App Constants
+class AppConstants {
+  static const String appName = 'Weather App';
+  static const int requestTimeout = 30;
+}
